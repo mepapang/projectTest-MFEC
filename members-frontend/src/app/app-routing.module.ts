b0b8b9cd282344a4f848171522dec1c0b37ca712
mepapang <pang.thunya@gmail.com>
2020-05-01 21:32:39 +0700

    start project

--- members-frontend/src/app/app-routing.module.ts
+++ members-frontend/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/forms', pathMatch: 'full' },
+  // { path: 'forms', component: FormsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
