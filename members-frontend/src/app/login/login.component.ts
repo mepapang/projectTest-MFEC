69b5c3361d3332d124f31848ceb009c81381cf29
mepapang <pang.thunya@gmail.com>
2020-05-02 22:17:55 +0700

    update login page

--- members-frontend/src/app/login/login.component.ts
+++ members-frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  iconUser = faUser;
+  iconPassword = faLock;
+
+  form = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+
   }
 
 }
