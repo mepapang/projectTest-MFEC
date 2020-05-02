ac186f8346636e04a48800fbbd7a47644c96f2aa
mepapang <pang.thunya@gmail.com>
2020-05-02 12:07:07 +0700

    create components

--- members-frontend/src/app/app.module.ts
+++ members-frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MembersListComponent } from './members-list/members-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserProfileComponent,
+    MembersListComponent
   ],
   imports: [
     BrowserModule,
