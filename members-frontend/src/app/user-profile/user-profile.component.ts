5f5fbb79ca927e7a5f241fbc9f530e133f95e877
mepapang <pang.thunya@gmail.com>
2020-05-02 22:18:30 +0700

    update user-profile page

--- members-frontend/src/app/user-profile/user-profile.component.ts
+++ members-frontend/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faMale, faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -7,6 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
+  iconName = faMale;
+  iconUser = faUser;
+  iconPassword = faLock;
+  iconEmail = faEnvelope;
+
+  formEdit = new FormGroup({
+    nickname: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
   constructor() { }
 
   ngOnInit() {
