a7dac433534eb41e8ae9e70c7e5738acbd947f00
mepapang <pang.thunya@gmail.com>
2020-05-02 22:18:09 +0700

    update register page

--- members-frontend/src/app/register/register.component.ts
+++ members-frontend/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { faMale, faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  iconName = faMale;
+  iconUser = faUser;
+  iconPassword = faLock;
+  iconEmail = faEnvelope;
+
+  formRegister = new FormGroup({
+    nickname: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+
+  }
 }
