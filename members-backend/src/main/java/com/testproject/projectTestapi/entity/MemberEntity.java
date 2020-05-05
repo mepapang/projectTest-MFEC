d035c160f0297037adf03eec19454f7cd702600c
mepapang <pang.thunya@gmail.com>
2020-05-05 23:22:27 +0700

    update get member data from DB

---
+++ members-backend/src/main/java/com/testproject/projectTestapi/entity/MemberEntity.java
@@ -0,0 +1,73 @@
+package com.testproject.projectTestapi.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "members")
+@Embeddable
+public class MemberEntity implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7793061768741693924L;
+	
+	@Id
+	@Column(name = "idmembers")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer memberId;
+
+	@Column(name = "fname")
+	private String fname;
+	
+	@Column(name = "lname")
+	private String lname;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "tel")
+	private String tel;
+	
+	
+	public Integer getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(Integer memberId) {
+		this.memberId = memberId;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+}
