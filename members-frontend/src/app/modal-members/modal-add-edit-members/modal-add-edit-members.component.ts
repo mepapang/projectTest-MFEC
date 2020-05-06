8bdb1203d63ef4085e01d13c0a90115c89c3331b
mepapang <pang.thunya@gmail.com>
2020-05-07 00:49:46 +0700

    update modal for edit and add

--- members-frontend/src/app/modal-members/modal-add-edit-members/modal-add-edit-members.component.ts
+++ members-frontend/src/app/modal-members/modal-add-edit-members/modal-add-edit-members.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MemberApiService } from 'src/app/service/member-api.service';
 
 @Component({
   selector: 'app-modal-add-edit-members',
@@ -8,9 +10,49 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModalAddEditMembersComponent implements OnInit {
 
-  constructor(public activeModal: NgbActiveModal) { }
+  @Input() public dataMemberDetail: any;
+
+  formAddEdit = new FormGroup({
+    memberId: new FormControl({value: '', disabled: true}),
+    fname: new FormControl('', Validators.required),
+    lname: new FormControl('', Validators.required),
+    tel: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+  });
+
+  constructor(public activeModal: NgbActiveModal, private memberService: MemberApiService) { }
 
   ngOnInit() {
+    this.setData();
   }
+
+  // ----- method for set value in input ----------
+  setData() {
+    this.formAddEdit.patchValue({
+      memberId: this.dataMemberDetail.memberId,
+      fname: this.dataMemberDetail.fname,
+      lname: this.dataMemberDetail.lname,
+      tel: this.dataMemberDetail.tel,
+      email: this.dataMemberDetail.email
+    });
+  }
+
+  // -------- method for add and edit member to DB --------
+  onSubmit() {
+    if (this.formAddEdit.getRawValue().memberId !== null) {
+      this.memberService.updateMember(this.formAddEdit.getRawValue()).subscribe( result => {
+        if (result) {
+          this.activeModal.close();
+        }
+      });
+    } else {
+      this.memberService.createMember(this.formAddEdit.value).subscribe( data => {
+        if (data) {
+          this.activeModal.close();
+        }
+      });
+    }
+  }
+
 
 }
