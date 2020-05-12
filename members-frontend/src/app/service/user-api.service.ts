f9c74f90339eb71bdde665e2af592413fc6b29f7
mepapang <pang.thunya@gmail.com>
2020-05-12 23:11:50 +0700

    update user profile

--- members-frontend/src/app/service/user-api.service.ts
+++ members-frontend/src/app/service/user-api.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserApiService {
 
-  // private adminUrl = 'http://localhost:8080/pjApi/admin';
+  private userUrl = 'http://localhost:8080/pjApi/user';
 
   constructor(private httpClient: HttpClient) { }
 
-  // getAdminBoard(): Observable<string> {
-  //   return this.httpClient.get(this.adminUrl, { responseType: 'text' });
-  // }
+  getUserByUsername(username): any {
+    return this.httpClient.get(this.userUrl + '/' + username);
+  }
+
+  updateUserProfile(user): any {
+    return this.httpClient.put(this.userUrl + '/update', user);
+  }
 }
--- members-frontend/src/app/user-profile/user-profile.component.ts
+++ members-frontend/src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { faMale, faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TokenStorageService } from '../auth/token-storage.service';
+import { UserApiService } from '../service/user-api.service';
+import { User } from '../model/user.model';
+import { ToastService } from '../service/toast.service';
 
 
 @Component({
@@ -16,14 +20,65 @@ export class UserProfileComponent implements OnInit {
   iconEmail = faEnvelope;
 
   formEdit = new FormGroup({
-    nickname: new FormControl(''),
-    email: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl('')
+    nickname: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required)
   });
-  constructor() { }
+
+  userForm: User;
+  userName: string;
+  show = false;
+  autohide = true;
+  constructor(private token: TokenStorageService,
+              private userService: UserApiService,
+              public toastService: ToastService) { }
 
   ngOnInit() {
+    if (this.token.getToken()) {
+      this.userName = this.token.getUsername();
+      this.getUserProfile(this.userName);
+    }
   }
 
+  getUserProfile(userName: string) {
+    this.userService.getUserByUsername(userName).subscribe((data) => {
+      this.userForm = data;
+      this.formEdit.patchValue({
+        nickname: data.nickname,
+        email: data.email
+      });
+    });
+  }
+
+  onCancel() {
+    this.getUserProfile(this.userName);
+  }
+
+  onSubmit() {
+    this.userForm.nickname = this.formEdit.value.nickname;
+    this.userForm.email = this.formEdit.value.email;
+    this.userService.updateUserProfile(this.userForm).subscribe( (data) => {
+      if (data) {
+        this.showSuccessToast();
+      }
+    }, error => {
+      console.log('Error : ', error);
+      this.showDangerToast();
+    });
+  }
+
+  showSuccessToast() {
+    this.toastService.show('User profile updated successfully', {
+      classname: 'bg-success text-light',
+      delay: 3000 ,
+      autohide: true,
+    });
+  }
+
+  showDangerToast() {
+    this.toastService.show('Error! Can not update user profile.' , {
+      classname: 'bg-danger text-light',
+      delay: 3000 ,
+      autohide: true,
+    });
+  }
 }
