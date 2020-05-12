77b5061afbaa2946e2d5979053861b8d1a8ed97d
mepapang <pang.thunya@gmail.com>
2020-05-12 23:14:13 +0700

    update user service and controller

--- members-backend/src/main/java/com/testproject/projectTestapi/controller/UserController.java
+++ members-backend/src/main/java/com/testproject/projectTestapi/controller/UserController.java
@@ -1,23 +1,36 @@
 package com.testproject.projectTestapi.controller;
 
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.testproject.projectTestapi.entity.UserEntity;
+import com.testproject.projectTestapi.service.UserService;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class UserController {
-
 	
-	@GetMapping("/admin")
-	@PreAuthorize("hasRole('admin')")
-	public String adminAccess() {
-		return ">>> Admin Contents";
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping(path = "/user/{username}")
+	public Optional<UserEntity> getUserByUserName(@PathVariable("username") String username) {
+		return userService.getUserByUserName(username);
 	}
 	
+	@PutMapping(path = "/user/update")
+	public UserEntity updateUserProfile(@RequestBody UserEntity user) {
+		return userService.updateUserProfile(user);
+	}
 	
 	
 }
